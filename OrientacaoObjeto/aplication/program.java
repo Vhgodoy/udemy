0a214f2d76a2ce135ed2e756d090e2bfac832de6
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-03-09 04:33:41 -0300

    criado calculo triangulo

--- OrientacaoObjeto/aplication/program.java
+++ OrientacaoObjeto/aplication/program.java
@@ -21,16 +21,15 @@ public class program {
         x.a = sc.nextDouble();
         x.b = sc.nextDouble();
         x.c = sc.nextDouble();
-        double p1 = (x.a + x.b + x.c) / 2.0;
-        double AreaX = Math.sqrt(p1 *(p1 - x.a)*(p1 - x.b)* (p1 - x.c));
 
+        double AreaX = x.area();
 
         System.out.println("Enter the measure of entities.triangle Y");
         y.a = sc.nextDouble();
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
-        double p2 = (y.a + y.b + y.c) / 2.0;
-        double AreaY = Math.sqrt(p2 * (p2 - y.a)*(p2 - y.b)*(p2 - y.c));
+
+        double AreaY = y.area();
 
         System.out.printf("Triangle X area: %.4f%n", AreaX );
         System.out.printf("Triangle Y area: %.4f%n", AreaY );
