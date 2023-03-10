d0520e676033e622d0e16e05bae63d059d29bd89
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-03-10 02:51:58 -0300

    criado classe Product

---
+++ OrientacaoObjeto/aplication/program01.java
@@ -0,0 +1,25 @@
+package aplication;
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class program01 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
