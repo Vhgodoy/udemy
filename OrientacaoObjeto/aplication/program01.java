f4ca8198a75d82b50a522ded7daf0d5b33f6475b
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-03-10 04:12:02 -0300

    formataçao de objeto produto

--- OrientacaoObjeto/aplication/program01.java
+++ OrientacaoObjeto/aplication/program01.java
@@ -1,6 +1,7 @@
 package aplication;
 import entities.Product;
 
+import java.sql.SQLOutput;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -11,11 +12,18 @@ public class program01 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.print("name: ");
+        product.name = sc.nextLine();
 
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
 
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
 
-
-
+        System.out.println(product);
 
 
 
