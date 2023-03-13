4e2afcc9bc764722ac66ce3ba7b541d4bdb21963
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-03-13 15:50:32 -0300

    classe student criada

---
+++ OrientacaoObjeto/entities/E03student.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class E03student {
+
+    public String name;
+
+    public int firstNote;
+    public int secondNote;
+    public int thirdNote;
+
+    public int total () {
+        return firstNote + secondNote + thirdNote;
+
+    }
+}
