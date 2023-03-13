80afa14ada61482b9435f347b05feae764b2c6d4
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-03-13 19:38:56 -0300

    Exemplo statico 1

---
+++ OrientacaoObjeto/exemploStatico/Aplication/Program.java
@@ -0,0 +1,24 @@
+package exemploStatico.Aplication;
+
+import exemploStatico.Util.Calculator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String [] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        System.out.printf("Circumference: %.2f%n" , Calculator.circumference(radius));
+        System.out.printf("Volume: %.2f%n" , Calculator.volume(radius));
+        System.out.printf("Volume: %.2f%n" , Calculator.PI);
+
+        sc.close();
+    }
+}
