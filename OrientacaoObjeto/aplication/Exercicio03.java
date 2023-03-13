076d1e3ae83f98972616be3c598bdc0e97fb2625
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-03-13 16:44:34 -0300

    exercicio finalizado

--- OrientacaoObjeto/aplication/Exercicio03.java
+++ OrientacaoObjeto/aplication/Exercicio03.java
@@ -1,4 +1,29 @@
 package aplication;
 
+import entities.E03student;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio03 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        E03student student = new E03student();
+
+        student.name = sc.nextLine();
+        student.firstNote = sc.nextDouble();
+        student.secondNote = sc.nextDouble();
+        student.thirdNote = sc.nextDouble();
+
+        System.out.printf("FINAL GRADE = %.2f%n" , student.total());
+
+        double pass = student.total();
+        student.pass(pass);
+
+        sc.close();
+    }
 }
--- OrientacaoObjeto/entities/E03student.java
+++ OrientacaoObjeto/entities/E03student.java
@@ -4,12 +4,23 @@ public class E03student {
 
     public String name;
 
-    public int firstNote;
-    public int secondNote;
-    public int thirdNote;
+    public double firstNote;
+    public double secondNote;
+    public double thirdNote;
 
-    public int total () {
+    public double total () {
         return firstNote + secondNote + thirdNote;
+    }
 
+
+
+    public void pass (double total) {
+        if (total >= 60) {
+            System.out.println("PASS");
+        } else {
+            double MISS = 60 - total ;
+            System.out.println("FAILED");
+            System.out.printf("MISSING %.2f POINTS" , MISS);
+        }
     }
 }
