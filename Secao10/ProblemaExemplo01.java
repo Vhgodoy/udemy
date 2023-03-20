3330f088d1851e862b0ca517a59364319c9ddda7
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-03-20 15:13:23 -0300

    vetor

---
+++ Secao10/ProblemaExemplo01.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProblemaExemplo01 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0.0;
+        for (int i=0 ; i<n; i++) {
+            sum += vect[i];
+        }
+        double avg = sum / n;
+
+        System.out.printf("AVERAGE HEIGHT: %.2f%n",avg);
+
+          //double media = (vect[0] + vect[1] + vect[2]) / 3;
+
+          //System.out.printf("AVERAGE HEIGHT = %.2f",media);
+
+
+
+        sc.close();
+    }
+}
