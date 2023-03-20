f391dd674652bc435f7023dd003d0a22619db97d
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-03-20 17:48:35 -0300

    vertor com classe

--- Secao10/ProblemaExemplo02.java
+++ Secao10/ProblemaExemplo02.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class ProblemaExemplo02 {
+import entities.product01;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProblemaExemplo02 {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        product01[] vect = new product01[N];
+
+        for (int i=0; i< vect.length; i++) {
+            sc.nextLine();
+            System.out.println("Enter name product: ");
+            String name = sc.next();
+            System.out.println("Enter with a price:  ");
+            double price = sc.nextDouble();
+            vect[i] = new product01(name, price);
+        }
+
+        System.out.println();
+        System.out.println(Arrays.toString(vect));
+        System.out.println();
+
+        double sum = 0.0 ;
+        for (int i = 0; i< vect.length; i++ ) {
+            sum += vect[i].getPrice();
+        }
+
+        double avg = sum / N;
+
+        System.out.printf("AVERAGE: %.2f%n", avg);
+
+        sc.close();
+    }
 }
--- Secao10/entities/product01.java
+++ Secao10/entities/product01.java
@@ -1,2 +1,32 @@
-package entities;public class product01 {
+package entities;
+
+public class product01 {
+
+    private String name;
+    private double price;
+
+    public product01 (String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return name
+                +" "
+                + String.format("%.2f", getPrice());
+    }
+
 }
