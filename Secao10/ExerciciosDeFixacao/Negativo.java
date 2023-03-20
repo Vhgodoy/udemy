fd16941760533659e1cd85a766440c00ae5c4f05
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-03-20 18:49:35 -0300

    exercicio de fixação

---
+++ Secao10/ExerciciosDeFixacao/Negativo.java
@@ -0,0 +1,31 @@
+package ExerciciosDeFixacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Negativo {
+
+    public static void main (String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos numeros voce vai digitar? ");
+        int n = sc.nextInt();
+        int[] vect = new int[n];
+
+        for (int i=0; i < vect.length; i++) {
+            System.out.println("Digite um numero: ");
+            vect[i] = sc.nextInt();
+        }
+
+        System.out.println("NUMEROS NEGATIVOS:");
+
+        for (int i = 0 ; i < n; i ++) {
+
+        }
+
+
+        sc.close();
+    }
+}
