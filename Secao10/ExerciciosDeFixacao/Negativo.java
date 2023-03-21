56bb8ffe002f9e34889f6b91fa32ee728f8521fd
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-03-21 09:40:45 -0300

    vetor negativos

--- Secao10/ExerciciosDeFixacao/Negativo.java
+++ Secao10/ExerciciosDeFixacao/Negativo.java
@@ -1,5 +1,6 @@
 package ExerciciosDeFixacao;
 
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,18 +11,21 @@ public class Negativo {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Quantos numeros voce vai digitar? ");
+        System.out.print("Quantos numeros voce vai digitar? ");
         int n = sc.nextInt();
         int[] vect = new int[n];
 
         for (int i=0; i < vect.length; i++) {
-            System.out.println("Digite um numero: ");
+            System.out.print("Digite um numero: ");
             vect[i] = sc.nextInt();
         }
 
         System.out.println("NUMEROS NEGATIVOS:");
 
-        for (int i = 0 ; i < n; i ++) {
+        for (int i = 0 ; i < vect.length; i ++) {
+            if (vect[i] < 0 ) {
+                System.out.println(vect[i]);
+            }
 
         }
 
