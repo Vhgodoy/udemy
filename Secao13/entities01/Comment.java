a3e08932deb28ff23a37910e91c180c1217102b8
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-04-25 12:37:06 -0300

    exercicio resolvido 01

---
+++ Secao13/entities01/Comment.java
@@ -0,0 +1,20 @@
+package entities01;
+
+public class Comment {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Comment(String text) {
+        this.text = text;
+
+
+    }
+}
