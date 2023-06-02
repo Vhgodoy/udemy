3e733e1754157f0dbe96f958701e032f76f8c39f
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-06-02 12:43:15 -0300

    Exercicio resolvido 164

---
+++ secao14/exercicioResolvido164/entities/Circle.java
@@ -0,0 +1,30 @@
+package exercicioResolvido164.entities;
+
+import exercicioResolvido164.entities.enums.Color;
+
+public class Circle extends Shape {
+
+    private Double radius;
+
+    public Circle(Double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * Math.pow(radius,2);
+    }
+}
---
+++ secao14/exercicioResolvido164/entities/enums/Color.java
@@ -0,0 +1,9 @@
+package exercicioResolvido164.entities.enums;
+
+public enum Color {
+
+    BLACK,
+    BLUE,
+    RED;
+
+}
