d21b61b4aaf36a208df576a084b1efad94330f34
Vhgodoy <victorhugogodoy1991@gmail.com>
2023-09-19 09:29:05 -0300

    try whit resources

--- Secao17-TrabalhandoComArquivos/FileAndBufferedReader215.java
+++ Secao17-TrabalhandoComArquivos/FileAndBufferedReader215.java
@@ -7,13 +7,8 @@ public class FileAndBufferedReader215 {
     public static void main (String[] args) {
 
         String path = "C:\\temp\\ws-learning\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
             String line = br.readLine();
 
             while (line != null) {
@@ -23,19 +18,6 @@ public class FileAndBufferedReader215 {
         }
         catch (IOException e ) {
             System.out.println("Error: " + e.getMessage());
-        }
-        finally {
-            try {
-                if ( br != null) {
-                    br.close();
-                }
-                if ( fr != null) {
-                    fr.close();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
